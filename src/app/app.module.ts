d541e060f4df878a2859fab3bb10e2acfe04f6f2
Gustavo Paco <gustavopaco@gmail.com>
2021-08-30 19:08:13 -0300

    - Implementando Modulo Cursos para definir nivel de visibilidade de components. Caso seja necessario que outro modulo ou ate mesmo o o app.modulo.ts veja um component eh preciso Exporta-lo no modulo secundario. Entao sua visibilidade sera publica para o Modulo APP pai.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {CursosModule} from "./cursos/cursos.module";
+
 import {MeuPrimeiroComponent} from "./meu-primeiro/meu-primeiro.component";
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
@@ -12,6 +16,9 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
         MeuPrimeiro2Component
     ],
   imports: [
+    CursosModule,
+    FormsModule,
+    HttpClientModule,
     BrowserModule
   ],
   providers: [],
